e90da5b452f361fbe943443f1b76c18114c9a336
Simon Pannek <simon.pannek@tum.de>
2020-12-29 22:27:28 +0100

    Add base for competitor preview

--- src/_includes/assets/js/games.js
+++ src/_includes/assets/js/games.js
@@ -1,6 +1,8 @@
 let count = 1;
 
 async function pageActions() {
+    // nextCompetitor().then(res => document.getElementById("next").innerText = res);
+
     count += 10;
 
     boardInfoOwnGames(count).then(printGamelist);
