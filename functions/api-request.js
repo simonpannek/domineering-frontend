ed834cc33f79d1b17489d78e5a1bf7c94d0d7d8a
Simon Pannek <simon.pannek@tum.de>
2020-12-29 16:08:35 +0100

    Add api proxy
    Add node-fetch lib

---
+++ functions/api-request.js
@@ -0,0 +1,34 @@
+const fetch = require("node-fetch");
+
+exports.handler = async event => {
+    const addr = "dom.simon.rest";
+
+    const url = "http://" + addr + "/" + event.path.substring(event.path.lastIndexOf("/") + 1);
+
+    const url_param = Object.keys(event.queryStringParameters).map(function(k) {
+        return encodeURIComponent(k) + '=' + encodeURIComponent(event.queryStringParameters[k])
+    }).join('&');
+
+    console.log(url + "?" + url_param);
+
+    return fetch(url + "?" + url_param).then(res => {
+        return res.json();
+    }).then(json => {
+        console.log(json);
+
+        return {
+            statusCode: 200,
+            body: JSON.stringify(json).replace(/[\u007F-\uFFFF]/g, function(chr) {
+                return "\\u" + ("0000" + chr.charCodeAt(0).toString(16)).substr(-4)
+            })
+        };
+    }).catch(err => {
+        return {
+            statusCode: 200,
+            body: JSON.stringify({
+                title: "Error",
+                message: "Something went wrong."
+            })
+        };
+    });
+}
